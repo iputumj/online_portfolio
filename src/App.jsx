8bc6b52b24407151f0dbb8208889d6a90c9e2bff
iputumj <iputu.mj@gmail.com>
2023-02-05 13:57:00 +0700

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { Avatar } from 'components';
+import { Navbar } from 'components';
+import { About, Awards, Experience, Home, Interest, Skills } from 'pages';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// styles
+import './app.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <Avatar />
-    </div>
+    <>
+      <Router>
+        <div className='app'>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/experience' element={<Experience />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/interest' element={<Interest />} />
+            <Route path='/awards' element={<Awards />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
