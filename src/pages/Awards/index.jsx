e9cc7f09aa18767a1f06e2306ff9cf2eb8054462
iputumj <iputu.mj@gmail.com>
2023-02-10 20:23:50 +0700

    create awards page

--- src/pages/Awards/index.jsx
+++ src/pages/Awards/index.jsx
@@ -1,10 +1,36 @@
+import Label from 'components/atoms/Label';
 import React, { Component } from 'react';
+import { Trophy } from 'assets';
 
 // styles
 import './index.scss';
 
 export default class Awards extends Component {
   render() {
-    return <div>Awards</div>;
+    return (
+      <div className='awards'>
+        <Label type='title' text='Awards & Certifications' />
+        <div className='col'>
+          <img src={Trophy} alt='trophy' />
+          <Label text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, quam!' />
+        </div>
+        <div className='col'>
+          <img src={Trophy} alt='trophy' />
+          <Label text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, quam!' />
+        </div>
+        <div className='col'>
+          <img src={Trophy} alt='trophy' />
+          <Label text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, quam!' />
+        </div>
+        <div className='col'>
+          <img src={Trophy} alt='trophy' />
+          <Label text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, quam!' />
+        </div>
+        <div className='col'>
+          <img src={Trophy} alt='trophy' />
+          <Label text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem, quam!' />
+        </div>
+      </div>
+    );
   }
 }
